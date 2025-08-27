2aa987657b4c104d886f681b828eb24f9a5f76b0
Michał Dzieliński <dzielinski.michal@gmail.com>
2025-08-27 10:34:27 +0200

    initial - 3 empty apps

---
+++ heureka/settings.py
@@ -0,0 +1,95 @@
+from pathlib import Path
+
+#region DEPLOYMENT SETTINGS
+BASE_DIR = Path(__file__).resolve().parent.parent
+SECRET_KEY = "django-insecure-32xta0ci*g*3w#x87@cr(2uzy^)1e4ef0t-=6^_e25@7_a=b$o"
+DEBUG = True
+ALLOWED_HOSTS = []
+WSGI_APPLICATION = "heureka.wsgi.application"
+#endregion
+
+#region BASE SETTINGS
+
+AUTH_USER_MODEL = 'staff.CustomUser'
+
+ROOT_URLCONF = "heureka.urls"
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+#endregion 
+
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    
+    #custom apps
+    'clinic',
+    'services',
+    'staff',
+]
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR, 'templates'],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
+
+#region LANGUAGES & TIME
+LANGUAGE_CODE = "en-us"
+TIME_ZONE = "Europe/Berlin"
+USE_I18N = True
+USE_TZ = True
+#endregion
+
+#region STATIC & MEDIA FILES
+
+STATIC_URL = "static/"
+
+#endregion
+
+
