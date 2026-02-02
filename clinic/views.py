e751acbde401417262d080f90c70a3d7bcf7fe8e
Michał Dzieliński <dzielinski.michal@gmail.com>
2026-02-02 10:43:03 +0100

    sam paweł

--- clinic/views.py
+++ clinic/views.py
@@ -17,7 +17,7 @@ from staff.models import Person
 
 
 class IndexView(TemplateView):
-    template_name='clinic/indxxx2.html'
+    template_name='clinic/indxxx3.html'
 
 class ProceduryView(TemplateView):
     template_name='clinic/procedury.html'
